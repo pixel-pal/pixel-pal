3d89979f8e472d2c0fbd27068895667165698c48
Andres Gutierrez <agfeynman@gmail.com>
2022-05-25 02:16:27 +0100

    feat: finish initial server setup

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import helmet from 'helmet';
+// import morgan from 'morgan';
+import cors from 'cors';
+import path from 'path';
+import router from './lib/router';
+
+async function createApp() {
+  const app = express();
+  app.use(express.json());
+  app.use(helmet());
+  app.use(cors());
+  app.use(express.static('dist/app'));
+  app.use('/api', router);
+
+  app.get('*', (_req, res) => {
+    res.sendFile(path.join(__dirname, 'app/index.html'));
+  });
+  return app;
+}
+
+export default createApp;
