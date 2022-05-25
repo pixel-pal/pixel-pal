a1f414838af94cb42ab4853cf9a2c14eef40ea93
Jessica Wachtel <jlwachtel@gmail.com>
2022-05-24 22:37:45 -0400

    committing

--- src/lib/utils/errorHandling.ts
+++ src/lib/utils/errorHandling.ts
@@ -1,3 +1,13 @@
-// const errorHandler= (err, req, res, next){
+const errorHandler = (err, req, res, next) => {
+  const defaultErr = {
+    log: err,
+    status: 400,
+    message: { err: 'An error occurred' },
+ };
+    const errorObj = Object.assign({}, defaultErr, err);
+    console.log(errorObj.log);
+    return res.status(errorObj.status).json(errorObj.message);
+}
 
-// }
+
+export default errorHandler
