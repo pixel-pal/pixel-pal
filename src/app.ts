aa6288d9c85e342464f95777789c0c6277e3f5c7
Jane Park <park.jane.sun@gmail.com>
2022-05-25 19:20:33 -0700

    feat: configure socket.io connection on back end

--- src/app.ts
+++ src/app.ts
@@ -4,17 +4,33 @@ import helmet from 'helmet';
 import cors from 'cors';
 import path from 'path';
 import router from './lib/router';
+import http from 'http';
+import { Server } from 'socket.io';
+
 
 async function createApp() {
   const app = express();
+  const httpServer = http.createServer(app);
+  const io = new Server(httpServer);
+
+  //TODO: move to different file
+  io.on('connection', (socket) => {
+    console.log('User connected');
+    
+    socket.on('disconnect', () => {
+      console.log(`User disconnected!, ${socket}`);
+    });
+  });
+
   app.use(express.json());
   app.use(helmet());
   app.use(cors());
   app.use(express.static('dist/app'));
   app.use('/api', router);
 
+
   app.get('*', (_req, res) => {
-    res.sendFile(path.join(__dirname, 'app/index.html'));
+    res.sendFile(path.join(__dirname, '../index.html'));
   });
   return app;
 }
